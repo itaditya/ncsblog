4c850978e3fcb03d9e83b08c56df79e99d42ecfe
ankitjain28may <ankitjain28may77@gmail.com>
2016-08-13 11:49:11 +0530

    .htaccess

--- load_data.php
+++ load_data.php
@@ -4,7 +4,7 @@
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: GET, POST');
     header('Access-Control-Max-Age: 604800');
-    header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With')
+    header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
 // require_once 'database.php';
 // $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // if(!empty($_GET['q']))
